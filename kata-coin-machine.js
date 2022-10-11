0bb5edf903577ad6d51d7f1c42983aba54aaebed
Eduard Gavrila <eduard.gavrila@gousto.co.uk>
2022-10-11 18:43:10 +0100

    create CoinMachine class to incapsulate coin machine functionality

--- kata-coin-machine.js
+++ kata-coin-machine.js
@@ -1,53 +1,93 @@
-//Deliberately unsorted array of coins
-const coins = [{ value: 1, symbol: "p" }, { value: 200, symbol: "£" }, { value: 100, symbol: "£" }, { value: 50, symbol: "p" }, { value: 20, symbol: "p" }, { value: 2, symbol: "p" },]
+class CoinMachine {
+  coinTypes = [
+    { value: 1, symbol: "p" },
+    { value: 2, symbol: "p" },
+    { value: 5, symbol: "p" },
+    { value: 10, symbol: "p" },
+    { value: 20, symbol: "p" },
+    { value: 50, symbol: "p" },
+    { value: 100, symbol: "£" },
+    { value: 200, symbol: "£" },
+  ];
+  constructor(initialCoinStock) {
+    if (initialCoinStock) {
+      initialCoinStock.forEach((coin) => {
+        // We are not covering all types of invalidinput yet. For now we rely  at least on coin object having the correct shape
 
-//select the largest coin out of an array of coins
-const findLargestCoinValue = (value, coins) => {
-    //assume array unsorted so sort the array
-    const sortedCoins = coins.sort((c1, c2) => c2.value - c1.value);
-    return sortedCoins.find(coin => value >= coin.value);
-}
+        //For now we are testing if the coin symbol and value are corect.
+        //Is a coin type and symbol found in coin types?
 
-const isInteger = (value) => value % 1 === 0;
+        const foundCoinType = coinTypes.find(
+          (coinType) => coinType.value === value && coinType.symbol === symbol
+        );
 
-const parseChangeGiven = (change) =>
-    change.map(coinGroup =>
-        coinGroup.coin.symbol === "£" ?
-            (coinGroup.amount + " £" + coinGroup.coin.value / 100) :
-            coinGroup.amount + " " + coinGroup.coin.value + "p"
-    )
+        if (!foundCoinType) {
+          throw new Error(
+            "Machine can not accept loading coins in current format!"
+          );
+        }
+      });
 
+      this.coinStock = initialCoinStock;
+    } else {
+      this.coinStock = {};
+    }
+  }
 
-exports.giveChange = (amount) => {
+  static findLargestCoinValue = (value, coinStock) => {
+    //assume coinStock unsorted and  sort the array
+    const sortedCoins = coinStock.sort((c1, c2) =>c2.value - c1.value)
+
+    return sortedCoins.find((coin) => value >= coin.value)
+  }
+
+  static isInteger = (value) => value % 1 === 0;
+
+  //change will be expressed in amount of pennies.
+  static parseChangeGiven = (change) =>
+    change.map((coinGroup) =>
+      coinGroup.coin.symbol === "£"
+        ? coinGroup.amount + " £" + coinGroup.coin.value / 100
+        : coinGroup.amount + " " + coinGroup.coin.value + "p"
+    );
+
+  loadChange = (coinStock) => (this.coinStock = coinStock);
+
+  giveChange = (amount) => {
     const currencyFormatPattern = /^£{0,1}\d+\.{0,1}(\d*$)/g;
     const isValidFormat = currencyFormatPattern.test(amount);
     const hasPoundSign = amount[0] === "£";
 
     if (!isValidFormat) {
-        throw new Error("Incorrect input format!");
+      throw new Error("Incorrect input format!");
     }
 
     let strippedAmount = amount;
 
     if (hasPoundSign) {
-        strippedAmount = amount.substring(1);
+      strippedAmount = amount.substring(1);
     }
 
     let numericValue = Number.parseFloat(strippedAmount);
 
-    if (!isInteger(strippedAmount) || hasPoundSign) {
-        numericValue *= 100
-    };
+    if (!CoinMachine.isInteger(strippedAmount) || hasPoundSign) {
+      numericValue *= 100;
+    }
 
     let changeGiven = [];
 
     while (numericValue > 0) {
-        const largestCoin = findLargestCoinValue(numericValue, coins);
-        const coinCount = Math.floor(numericValue / largestCoin.value);
-        changeGiven.push({ coin: largestCoin, amount: coinCount })
-        numericValue %= largestCoin.value;
+      const largestCoin = CoinMachine.findLargestCoinValue(
+        numericValue,
+        this.coinStock
+      );
+      const coinCount = Math.floor(numericValue / largestCoin.value);
+      changeGiven.push({ coin: largestCoin, amount: coinCount });
+      numericValue %= largestCoin.value;
     }
 
-    return parseChangeGiven(changeGiven).join(", ");
+    return CoinMachine.parseChangeGiven(changeGiven).join(", ");
+  };
+}
 
-};
+module.exports = CoinMachine;
--- kata-coin-machine.test.js
+++ kata-coin-machine.test.js
@@ -1,25 +1,40 @@
-const { giveChange } = require("./kata-coin-machine");
+const  CoinMachine  = require("./kata-coin-machine");
 
-describe("Given giveChange function", () => {
-  describe("When calling giveChange()", () => {
+describe("Given coinMachine", () => {
+  let coinMachine;
+  beforeEach(() => {
+    coinMachine = new CoinMachine();
+    coinMachine.loadChange([
+      { value: 1, symbol: "p", amount: 30 },
+      { value: 2, symbol: "p", amount: 30 },
+      { value: 5, symbol: "p", amount: 20 },
+      { value: 10, symbol: "p", amount: 20 },
+      { value: 20, symbol: "p", amount: 10 },
+      { value: 50, symbol: "p", amount: 10 },
+      { value: 100, symbol: "£", amount: 10 },
+      { value: 200, symbol: "£", amount: 5 },
+    ]);
+  });
+
+  describe("When calling coinMachine.giveChange()", () => {
     test("Then it throws an error when input is not in acceptable format.", () => {
       //valid values
-      expect(() => giveChange("£2")).not.toThrow();
-      expect(() => giveChange("253")).not.toThrow();
-      expect(() => giveChange("5.23")).not.toThrow();
+      expect(() => coinMachine.giveChange("£2")).not.toThrow();
+      expect(() => coinMachine.giveChange("253")).not.toThrow();
+      expect(() => coinMachine.giveChange("5.23")).not.toThrow();
       //invalid values
-      expect(() => giveChange("$23")).toThrow();
-      expect(() => giveChange("")).toThrow();
-      expect(() => giveChange("0.25g")).toThrow();
-      //expect(() => giveChange("5.")).toThrow();
+      expect(() => coinMachine.giveChange("$23")).toThrow();
+      expect(() => coinMachine.giveChange("")).toThrow();
+      expect(() => coinMachine.giveChange("0.25g")).toThrow();
+      //expect(() => coinMachine.giveChange("5.")).toThrow();
     });
   });
 
-  describe("When calling giveGhange() with correct input", () => {
+  describe("When calling coinMchine.giveGhange() with correct input", () => {
     test("Then in returns the correct change.", () => {
-      expect(giveChange("£2")).toEqual("1 £2");
-      expect(giveChange("253")).toEqual("1 £2, 1 50p, 1 2p, 1 1p");
-      expect(giveChange("5.23")).toEqual("2 £2, 1 £1, 1 20p, 1 2p, 1 1p");
+      expect(coinMachine.giveChange("£2")).toEqual("1 £2");
+      expect(coinMachine.giveChange("253")).toEqual("1 £2, 1 50p, 1 2p, 1 1p");
+      expect(coinMachine.giveChange("5.23")).toEqual("2 £2, 1 £1, 1 20p, 1 2p, 1 1p");
     });
   });
 });
